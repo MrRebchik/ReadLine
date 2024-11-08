61bf73c80e2cf8cbe4635d3f65313f08dd3bc363
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-11-08 10:27:45 +0300

    Удалили лишние начальные файлы

--- ReadLine/Program.cs
+++ ReadLine/Program.cs
@@ -1,8 +1,5 @@
 using ReadLine.Models;
-using Microsoft.EntityFrameworkCore;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
-using Microsoft.Extensions.Options;
-using ReadLine.Models.People;
+using Microsoft.EntityFrameworkCore;using ReadLine.Models.People;
 using Microsoft.AspNetCore.Identity;
 
 namespace ReadLine
@@ -15,10 +12,13 @@ namespace ReadLine
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
             builder.Services.AddDbContext<MainDataContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("MainDataConnection")); options.EnableSensitiveDataLogging(true); });
             builder.Services.AddDbContext<ModerateDataContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("ModerateDataConnection")); options.EnableSensitiveDataLogging(true); });
             builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
             builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
+
+            builder.Services.AddControllers();
 
             var app = builder.Build();
 
@@ -33,10 +33,6 @@ namespace ReadLine
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
 
             app.Run();
 
