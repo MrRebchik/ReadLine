8d614b8d8034b89a8ff49419eb8749e56618cfa4
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-11-17 18:47:13 +0300

    реализовал поиск книг по названию

---
+++ ReadLine/Controllers/FormController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReadLine.Models;
+
+namespace ReadLine.Controllers
+{
+    public class FormController : Controller
+    {
+        private MainDataContext context { get; set; }
+        public FormController (MainDataContext mainDataContext)
+        {
+            this.context = mainDataContext;
+        }
+        public async Task<IActionResult> Index(long id = 1)
+        {
+            return View("Form", await context.Books.FindAsync(id));
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm()
+        {
+            foreach(string key in Request.Form.Keys.Where(k => !k.StartsWith("_")))
+            {
+                TempData[key] = string.Join(", ", Request.Form[key]);
+            }
+            return RedirectToAction(nameof(Results));
+        }
+        public IActionResult Results()
+        {
+            return View(TempData);
+        }
+    }
+}
--- ReadLine/Controllers/HomeController.cs
+++ ReadLine/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReadLine.Models;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ReadLine.Controllers
 {
@@ -8,15 +10,67 @@ namespace ReadLine.Controllers
     {
         private MainDataContext context;
         public HomeController(MainDataContext context) { this.context = context; }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(IEnumerable<Book> booksSearch = null)
         {
-            var books = await context.Books.Include(b => b.Author).ToListAsync();
+            IEnumerable<Book> books;
+            if (booksSearch == null)
+            {
+                books = await context.Books.Include(b => b.Author).ToListAsync();
+            }
+            else
+            {
+                books = booksSearch;
+            }
             return View(context.Books.ToList());
         }
         public IActionResult OpenBookPage()
         {
             long id = long.Parse(HttpContext.Request.Query["itemid"]);
             return Redirect($"/BookInfo/Index/{id}");
+        }
+
+        [HttpPost]
+        public IActionResult SearchForm()
+        {
+            foreach (string key in Request.Form.Keys.Where(k => !k.StartsWith("_")))
+            {
+                TempData[key] = string.Join(", ", Request.Form[key]);
+            }
+            return RedirectToAction(nameof(Results));
+        }
+        public async Task<IActionResult> Results()
+        {
+
+            IEnumerable<Book> books = await context.Books.Include(b => b.Author).ToListAsync();
+            IEnumerable<Book> result = books.
+                Where(b => 
+                TempData["search-string"].ToString().ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries).Intersect(b.Title.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries)).Count()>0).
+                ToList();
+            return View("Index", result);
+        }
+        //private List<Book> AssertFilter(Book b, string filter)
+        //{
+        //    string[] inputs = filter.Split([' ', ',', '.']);
+        //    int matchCount = 0;
+        //    foreach (var item in inputs)
+        //    {
+        //        Regex regex = new Regex(item.Substring(0,item.Length-2) + "(\\w*)", RegexOptions.IgnoreCase); //TODO если строка длиной 1
+        //        MatchCollection titleMatches = regex.Matches(b.Title);
+        //        MatchCollection authorMatches = regex.Matches(b.Author.Name);
+        //        matchCount += titleMatches.Count;
+        //        matchCount += authorMatches.Count;
+        //    }
+        //    return matchCount > 0;
+        //}
+        public List<Book> SearchProducts(string pattern)
+        {
+            // Компиляция регулярного выражения
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            // Фильтрация товаров по совпадению с регулярным выражением
+            var filteredProducts = context.Books.Where(p => regex.IsMatch(p.Title)).ToList();
+
+            return filteredProducts;
         }
     }
 }
