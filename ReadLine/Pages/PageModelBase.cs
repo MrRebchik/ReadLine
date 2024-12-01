2dfd591e2a878441ff12db5a518d335021a41f09
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-12-01 12:26:56 +0300

    Сделал базовый класс контроллеров

---
+++ ReadLine/Pages/PageModelBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ReadLine.Models.People;
+
+namespace ReadLine.Pages
+{
+    public class PageModelBase : PageModel
+    {
+        public UserManager<IdentityUser> UserManager;
+        public ProfileContext ProfileContext;
+        public PageModelBase(UserManager<IdentityUser> usrManager, ProfileContext profileContext)
+        {
+            UserManager = usrManager;
+            ProfileContext = profileContext;
+        }
+    }
+}
