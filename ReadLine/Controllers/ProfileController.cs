dd0c143b71676776854ddbc6ea90d4ae496d293b
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2025-02-24 14:30:18 +0300

    Сделал добавление книг в профиле
    Нужно добавить проверку на наличие

--- ReadLine/Controllers/ProfileController.cs
+++ ReadLine/Controllers/ProfileController.cs
@@ -9,12 +9,21 @@ namespace ReadLine.Controllers
 {
     public class ProfileController : BookControllerBase
     {
+        public UserProfile Profile { get; set; }
         private ProfileContext ProfileContext;
+        private MainDataContext MainDataContext;
+        private List<Book> Books;
+        private List<UserProfile> UserProfiles;
         public ProfileController(MainDataContext mainDataContext, ProfileContext profileContext) : base(mainDataContext)
         {
             ProfileContext = profileContext;
+            MainDataContext = mainDataContext;
+            Books = MainDataContext.Books.Include(b => b.Author).Include(b => b.Tags).Include(b => b.Category).ToList();
+            UserProfiles = ProfileContext.UserProfiles
+                    .Include(b => b.ReadBooks)
+                    .Include(b => b.FavoriteBooks)
+                    .Include(b => b.WishBooks).ToList();
         }
-        public UserProfile Profile { get; set; }
         public async Task<IActionResult> Index()
         {
             ProfileViewModel profileVM = new ProfileViewModel();
@@ -24,6 +33,30 @@ namespace ReadLine.Controllers
                 profileVM = new ProfileViewModel(Profile) ;
             }
             return View(profileVM);
+        }
+        public async Task<IActionResult> AddBook(long id, [FromQuery] string selectedList)
+        {
+            ProfileViewModel profileVM = new ProfileViewModel();
+            if (User.Identity.IsAuthenticated)
+            {
+                Profile = await ProfileContext.UserProfiles.FindAsync(User.Identity.Name);
+                Book book = MainDataContext.Books.First(b => b.BookId == id);
+                switch (selectedList.ToLower())
+                {
+                    //сначала проверить что такой книги еще нет
+                    case "readbooks":
+                        Profile.ReadBooks.Add(book);
+                        break;
+                    case "wishbooks":
+                        Profile.WishBooks.Add(book);
+                        break;
+                    case "favoritebooks":
+                        Profile.FavoriteBooks.Add(book);
+                        break;
+                }
+                profileVM = new ProfileViewModel(Profile);
+            }
+            return View("Index", profileVM);
         }
     }
 }
--- ReadLine/Models/People/UserProfile.cs
+++ ReadLine/Models/People/UserProfile.cs
@@ -10,9 +10,9 @@ namespace ReadLine.Models.People
         public string IdentityUserName { get; set; }
         public IdentityUser IdentityUser { get; set; }
         public string? ProfileQuote { get; set; }
-        public List<Book> ReadBooks { get; set; } = [];
-        public List<Book> FavoriteBooks { get; set; } = [];
-        public List<Book> WishBooks { get; set; } = [];
-        public List<UserProfile> Friends { get; set; } = [];
+        public List<Book> ReadBooks { get; set; }
+        public List<Book> FavoriteBooks { get; set; }
+        public List<Book> WishBooks { get; set; }
+        public List<UserProfile> Friends { get; set; }
     }
 }
